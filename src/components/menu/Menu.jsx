bb123ebf13a9b4b7dfa5f2a4c5791c61e03779f0
Jean Trujillo <91339890+jeantpdev@users.noreply.github.com>
2024-04-19 14:08:24 -0500

    Add files via upload

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -22,8 +22,8 @@ export default function Menu({color = "text-white"}) {
                     <div className='flex justify-between py-5 px-10 items-center'>
                         <h1 className='text-2xl md:text-3xl font-bold uppercase'><span className='text-purple-400'>lilac</span>dreamers</h1>
                         <button onClick={toggleMenu} className="text-gray-500 hover:text-gray-600 focus:outline-none focus:text-gray-600">
-                            <svg fill="#000000" className='h-16 w-16' version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
-                                viewBox="0 0 460.775 460.775" xml:space="preserve">
+                            <svg fill="#000000" className='h-16 w-16' version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" 
+                                viewBox="0 0 460.775 460.775" xmlSpace="preserve">
                                 <path d="M285.08,230.397L456.218,59.27c6.076-6.077,6.076-15.911,0-21.986L423.511,4.565c-2.913-2.911-6.866-4.55-10.992-4.55
                                     c-4.127,0-8.08,1.639-10.993,4.55l-171.138,171.14L59.25,4.565c-2.913-2.911-6.866-4.55-10.993-4.55
                                     c-4.126,0-8.08,1.639-10.992,4.55L4.558,37.284c-6.077,6.075-6.077,15.909,0,21.986l171.138,171.128L4.575,401.505
